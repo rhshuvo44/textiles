bb71f6d218338e93e01d90aa407d5c33433d40e4
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-11-18 13:44:05 +0600

    services pages partner ,stat and newsletter

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,6 +1,9 @@
 import HomeServices from "@/components/Modules/home/HomeServices";
+import Stats from "@/components/Modules/home/Stats";
 import ServiceCapabilities from "@/components/Modules/services/ServiceCapabilities";
 import WeDo from "@/components/Modules/services/WeDo";
+import NewsLatest from "@/components/UI/NewsLatest";
+import PartnerSlide from "@/components/UI/PartnerSlide";
 import SectionTitle from "@/components/UI/SectionTitle";
 
 const ServicePage = () => {
@@ -10,6 +13,9 @@ const ServicePage = () => {
       <HomeServices />
       <ServiceCapabilities />
       <WeDo />
+      <PartnerSlide />
+      <Stats />
+      <NewsLatest />
     </>
   );
 };
