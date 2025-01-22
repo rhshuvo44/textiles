78e7c1aaa0fe69c2c80eb1049ff48c2bf20fa361
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2025-01-22 16:37:39 +0600

    remove:api and product type

--- src/types/index.ts
+++ src/types/index.ts
@@ -16,46 +16,7 @@ export type TContactForm = {
     phone: string
     message: string
 }
-export interface Review {
-    rating: number
-    comment: string
-    date: string
-    reviewerName: string
-    reviewerEmail: string
-}
-export type TProduct = {
-    id: number;
-    title: string;
-    description: string;
-    price: number;
-    discountPercentage: number;
-    rating: number;
-    stock: number;
-    availabilityStatus: string;
-    minimumOrderQuantity: number;
-    shippingInformation: string;
-    returnPolicy: string;
-    warrantyInformation: string;
-    brand: string;
-    category: string;
-    tags: string[];
-    dimensions: {
-        width: number;
-        height: number;
-        depth: number;
-    };
-    weight: number;
-    sku: string;
-    meta: {
-        createdAt: string;
-        updatedAt: string;
-        barcode: string;
-        qrCode: string;
-    };
-    thumbnail: string;
-    images: string[];
-    reviews: Review[];
-};
+
 
 
 
